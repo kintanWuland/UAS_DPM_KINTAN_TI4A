c5dcbbe1e57f49a6e7a653a2b2101b61830dcc1a
kintanWuland <wulandarikintan0@gmail.com>
2022-06-17 19:44:15 +0700

    Initial commit

---
+++ app/src/main/java/com/example/uas_dpm_kintan_ti4a/DataDosen.kt
@@ -0,0 +1,11 @@
+package com.example.uas_dpm_kintan_ti4a
+
+class DataDosen (
+    var nidn: String,
+    var namaDosen: String,
+    var jabatan: String,
+    var golongan: String,
+    var pendidikan: String,
+    var keahlian: String,
+    var programstudi: String
+)
